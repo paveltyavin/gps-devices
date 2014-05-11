37a868ae1ad3ee9fd13f30e4cac11968e104bc00
Pavel Tyavin <pavel.tyavin@msk.action.ru>
2014-05-11 22:59:14 +0400

    GP2000X readme

--- devices/GP2000X/README.md
+++ devices/GP2000X/README.md
@@ -1,1 +1,22 @@
 # GP2000X
+
+## Message format
+NMEA 0183 format:
+`"$GPRMC,162017.000,A,5551.5039,N,03740.0065,E,0.00,,040912,0,,A*75+79123456789#"`
+
+## Setup
+
+Set your phone as admin phone
+```*ADMIN+79121234567#0000#```
+
+GPRS setup. 021.012.123.021 is server ip, 9102 - tcp port, 0000 - password
+```*HOSIP021.012.123.021#9102#0000#```
+
+Set APN name
+```*AT+CSTT=internet#```
+
+Set GPRS mode
+```*WMODE1011#0000#```
+
+Setup tracker to send gps info every 30 seconds
+```*AUTOT02#30S#0000#```
--- devices/GP2000X/server.js
+++ devices/GP2000X/server.js
@@ -12,7 +12,7 @@ net.createServer(function (socket) {
   socket.setEncoding("utf8");
   socket.addListener('data', function (message) {
     logger.info(message);
-    // message is like "$GPRMC,162017.000,A,5551.5039,N,03740.0065,E,0.00,,040912,0,,A*75+79262638433#"
+    // message is like "$GPRMC,162017.000,A,5551.5039,N,03740.0065,E,0.00,,040912,0,,A*75+79123456789#"
     var re = /\$GPRMC,.*,A,(\d{2})(\d{2})\.(\d{4}),N,0(\d{2})(\d{2})\.(\d{4}),E.*#/;
     var res = re.exec(message);
     if (!res) {
